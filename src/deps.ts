b79265ad0d3e809d1ab16566cf5c964596a9c564
Charles Lowell <cowboyd@frontside.com>
2022-07-11 12:15:56 -0500

    basic configurable field-level generation

---
+++ src/deps.ts
@@ -0,0 +1,3 @@
+export * as graphql from "https://esm.sh/graphql@16.5.0/graphql";
+export * from "https://deno.land/x/continuation/mod.ts";
+export { assert } from "https://deno.land/std@0.145.0/testing/asserts.ts";
