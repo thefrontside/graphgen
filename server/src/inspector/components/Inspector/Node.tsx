0b8462f60e4101fa62ccf1e1006b7394c9c31be5
Paul Cowan <paul.cowan@cutting.scot>
2022-09-11 18:08:45 +0100

    very basic styling

--- server/src/inspector/components/Inspector/Node.tsx
+++ server/src/inspector/components/Inspector/Node.tsx
@@ -23,55 +23,51 @@ export function Node(
   );
 
   return (
-    <div className="sub-meta" style={{ display: "inline-block" }}>
-      <table>
-        <tbody>
-          {props
-            .map((n) => (
-              <tr key={n.key}>
-                <td>{n.key}</td>
-                <td>{n.json as string}</td>
-              </tr>
-            ))}
-          {relationships.map((r) => {
-            const index = node.fields.findIndex(field => r.key === field.key);
-            const path = /\|data$/.test(parentId)
-              ? `${parentId}.${r.key}`
-              : `${parentId}.fields.${index}`;
-            let id = path;
+    <div className="node" style={{ display: "inline-block" }}>
+      {props
+        .map((n) => (
+          <div className="field">
+            <div className="th">{n.key}</div>
+            <div className="td">{n.json as string}</div>
+          </div>
+        ))}
+      {relationships.map((r) => {
+        const index = node.fields.findIndex((field) => r.key === field.key);
+        const path = /\|data$/.test(parentId)
+          ? `${parentId}.${r.key}`
+          : `${parentId}.fields.${index}`;
+        let id = path;
 
-            if (r.__typename === "VertexFieldEntry") {
-              id += `.${r.__typename}.${r.id}`;
-            } else if (r.__typename === "VertexListFieldEntry") {
-              id += `.${r.__typename}.${r.ids.join(",")}`;
-            } else {
-              throw new Error(`illegal FieldEntry`);
-            }
+        if (r.__typename === "VertexFieldEntry") {
+          id += `.${r.__typename}.${r.id}`;
+        } else if (r.__typename === "VertexListFieldEntry") {
+          id += `.${r.__typename}.${r.ids.join(",")}`;
+        } else {
+          throw new Error(`illegal FieldEntry`);
+        }
 
-            return (
-              <TreeItem key={id} nodeId={id} label={r.key}>
-                {r.data && r.__typename === "VertexFieldEntry"
-                  ? (
-                    <TreeItem
-                      key={r.data.id}
-                      nodeId={r.data.id}
-                      label={<Node parentId={`${path}.data`} node={r.data} />}
-                    />
-                  )
-                  : r.__typename === "VertexListFieldEntry" && !!r.data
-                  ? r.data.map((n, i) => (
-                    <TreeItem
-                      key={n.id}
-                      nodeId={n.id}
-                      label={<Node parentId={`${path}.data.${i}`} node={n} />}
-                    />
-                  ))
-                  : <Loader />}
-              </TreeItem>
-            );
-          })}
-        </tbody>
-      </table>
+        return (
+          <TreeItem key={id} nodeId={id} label={r.key}>
+            {r.data && r.__typename === "VertexFieldEntry"
+              ? (
+                <TreeItem
+                  key={r.data.id}
+                  nodeId={r.data.id}
+                  label={<Node parentId={`${path}.data`} node={r.data} />}
+                />
+              )
+              : r.__typename === "VertexListFieldEntry" && !!r.data
+              ? r.data.map((n, i) => (
+                <TreeItem
+                  key={n.id}
+                  nodeId={n.id}
+                  label={<Node parentId={`${path}.data.${i}`} node={n} />}
+                />
+              ))
+              : <Loader />}
+          </TreeItem>
+        );
+      })}
     </div>
   );
 }
