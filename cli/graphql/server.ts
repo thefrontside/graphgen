bfc950cf1d19e5cc9323998ca154f5081c6ba99e
Paul Cowan <paul.cowan@cutting.scot>
2022-09-14 12:40:42 +0100

    add graphql server
    Signed-off-by: Paul Cowan <paul.cowan@cutting.scot>

--- cli/graphql/server.ts
+++ cli/graphql/server.ts
@@ -1,9 +1,36 @@
-import { serve, serveStatic, Hono } from '../deps.ts';
+import { serve, serveStatic, Hono, createServer } from '../deps.ts';
+import { resolvers, typeDefs } from "./schema.ts";
+import { makeContext } from './context.ts';
+
+const PORT = Number(Deno.env.get('PORT') ?? 8000);
+
+export const graphQLServer = createServer({
+  schema: {
+    typeDefs,
+    resolvers
+  },
+  maskedErrors: false,
+  context: makeContext()
+})
 
 const app = new Hono();
 
 export async function main() {
+  app.use('/graphql', (ctx) => {
+    return graphQLServer.handleRequest(ctx.req, ctx.res);
+  })
+
   app.use('*', serveStatic({ root: './', path: 'dist' }));
-  
-  await serve(app.fetch);
+
+  await serve(app.fetch, {
+    port: PORT, onListen({ port, hostname }) {
+      console.log(`Server started at http://${hostname}:${port}`);
+    },
+    onError(err) {
+      console.error(err);
+      return new Response("Internal Server Error", { status: 500 });
+    }
+  });
 }
+
+await main();
