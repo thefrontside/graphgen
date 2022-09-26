4e3c271bbbeadea042c5476724d3471a78480f2d
Paul Cowan <paul.cowan@cutting.scot>
2022-09-26 16:25:38 +0100

    conditionally add watch in vite.config

--- cli/app/src/components/Topbar/Topbar.tsx
+++ cli/app/src/components/Topbar/Topbar.tsx
@@ -4,7 +4,7 @@ export function Topbar(): JSX.Element {
   return (
     <section className="top-bar">
       <ul>
-        <li><h1>We need a Graphgen logo!</h1></li>
+        <li><h1>Graphgen</h1></li>
         <li><a target="_blank" href="/graphql">GraphiQL</a></li>
       </ul>
     </section>
