080532599065be522d40b298d0ab1f01c892ce40
Taras <tarasm@gmail.com>
2022-12-12 19:40:10 -0500

    Added graphgen base url to astro and netlify

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -4,11 +4,13 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		// Enable Preact to support Preact JSX components.
-		preact(),
-		// Enable React for the Algolia search component.
-		react(),
-	],
-	site: `http://astro.build`,
+  integrations: [
+    // Enable Preact to support Preact JSX components.
+    preact(),
+    // Enable React for the Algolia search component.
+    react(),
+  ],
+  site: "https://graphgen.netlify.app",
+  base: "/graphgen",
+  trailingSlash: "always",
 });
