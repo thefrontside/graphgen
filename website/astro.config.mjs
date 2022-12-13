2c5a9d74f705a54c55a00263e2a16339103000d9
Taras <tarasm@gmail.com>
2022-12-12 20:19:26 -0500

    Use github prime url

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -6,7 +6,7 @@ let site = "https://graphgen.netlify.app";
 if (process.env.CONTEXT === "dev") {
 	site = "http://localhost:3000";
 } else if (process.env.CONTEXT === "deploy-preview") {
-	site = process.env.DEPLOY_URL;
+	site = process.env.DEPLOY_PRIME_URL;
 }
 
 // https://astro.build/config
