24c1cc5721a77611f8348489907f1b6b885bc12c
Charles Lowell <cowboyd@frontside.com>
2022-12-21 09:11:13 -0600

    Remove GraphQL pin to facilitate NPM build

--- src/deps.ts
+++ src/deps.ts
@@ -1,4 +1,4 @@
-export * as graphql from "https://esm.sh/graphql@16.5.0/graphql?pin=v95";
+export * as graphql from "https://esm.sh/graphql@16.5.0/graphql";
 export * from "https://deno.land/x/continuation@0.1.0/mod.ts";
 export * from "https://deno.land/x/seed@1.0.0/index.ts";
 export { assert } from "https://deno.land/std@0.145.0/testing/asserts.ts";
